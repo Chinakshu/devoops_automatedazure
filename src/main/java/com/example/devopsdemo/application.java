2d1f73fdeb5b795b8c11f8a58e684ecafb2156d6
Chinakshu <chinakshu2005@gmail.com>
2025-07-24 01:04:40 +0530

    Initial project

---
+++ src/main/java/com/example/devopsdemo/application.java
@@ -0,0 +1,7 @@
+package com.example.devopsdemo;
+
+public class DemoApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello from Fake DevOps CI/CD Demo!");
+    }
+}
